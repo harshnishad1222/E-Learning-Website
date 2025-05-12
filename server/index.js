1516e65f8fa01c618ff7ebf89ce1de4c81451ad5
Harsh Nishad <90214645+harshnishad1222@users.noreply.github.com>
2025-05-13 01:43:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://e-learning-website-cop3v4n1r-harsh-nishads-projects-4daddf71.vercel.app"],
+    origin: ["http://localhost:5173", "https://e-learning-website-f.onrender.com"],
     credentials: true
 }));
 
