81e14e99598891f6ab714e218bd08f2215c6ea61
Harsh Nishad <90214645+harshnishad1222@users.noreply.github.com>
2025-05-13 01:16:08 +0530

    Update LectureTab.jsx

--- client/src/pages/admin/lecture/LectureTab.jsx
+++ client/src/pages/admin/lecture/LectureTab.jsx
@@ -17,7 +17,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 
-const MEDIA_API = "http://localhost:8087/api/v1/media";
+const MEDIA_API = "https://e-learning-website-b8sl.onrender.com/api/v1/media";
 
 const LectureTab = () => {
   const [lectureTitle, setLectureTitle] = useState("");
