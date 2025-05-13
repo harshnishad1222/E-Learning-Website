a1fb3a9f17c37e5f0bf82e21ae6534bde4146f44
Harsh Nishad <90214645+harshnishad1222@users.noreply.github.com>
2025-05-13 13:36:54 +0530

    Update authApi.js

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -34,7 +34,7 @@ export const authApi = createApi({
         try {
           const result = await queryFulfilled;
           dispatch(userLoggedIn({ user: result.data.user }));
-                window.location.reload();
+              authApi.util.invalidateTags(["Profile"]);
 
         } catch (error) {
           console.error("Login failed:", error);
