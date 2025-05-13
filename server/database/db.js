d8d2422f35d4336a52b92238417b0fdf7bdf53d3
Harsh Nishad <90214645+harshnishad1222@users.noreply.github.com>
2025-05-13 10:52:36 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,17 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1); // Exit process with failure
-  }
-};
-
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log ('MongoDb connected');
+    } catch (error){
+        console.log("error occured", error);
+    }
+}
 export default connectDB;
