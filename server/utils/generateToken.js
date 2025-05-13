8048bc2de8b3005405ed7aeb770d377883ba30f1
Harsh Nishad <90214645+harshnishad1222@users.noreply.github.com>
2025-05-13 11:17:05 +0530

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,17 +1,22 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 export const generateToken = (res, user, message) => {
-    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1d', });
+  const token = jwt.sign(
+    { userId: user._id },
+    process.env.SECRET_KEY,
+    { expiresIn: "1d" }
+  );
 
-    return res.status(200).cookie("jwt", token,
-         { 
-        httpOnly: true, 
-        sameSite: 'strict',
-        maxAge: 24 * 60 * 60 * 1000, 
-    }).json({
-        success:true,
-        message,
-        user
-    });
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    sameSite: "strict",
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+    // secure: true, // Uncomment this in production with HTTPS
+  });
 
+  return res.status(200).json({
+    success: true,
+    message,
+    user,
+  });
 };
